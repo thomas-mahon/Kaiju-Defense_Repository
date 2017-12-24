68de9e641b6f94675b705e099cbc65b9b41cc986
lex116 <lexus.lopez@loop.colum.edu>
2017-12-23 22:48:08 -0600

    Trim

--- KD_Prototype/Assets/KD_Assets/TemporaryMovement.cs
+++ KD_Prototype/Assets/KD_Assets/TemporaryMovement.cs
@@ -4,55 +4,50 @@ using UnityEngine;
 
 public class TemporaryMovement : MonoBehaviour
 {
-    Rigidbody rb;
-    float MovementForce = 5000;
-    float RotationSpeed = 80;
+    //Rigidbody rb;
+    //float MovementForce = 5000;
+    //float RotationSpeed = 80;
+    //bool isBeingControlled;
 
-    bool isBeingControlled;
-    public GameObject camera;
+
 
 	// Use this for initialization
 	void Start ()
     {
-        rb = GetComponent<Rigidbody>();
-
-        ToggleControl(false);
+        //rb = GetComponent<Rigidbody>();
+        //ToggleControl(false);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (isBeingControlled == true)
-        {
-            #region
+        //if (isBeingControlled == true)
+        //{
+        //    #region
 
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Rotate(transform.up * -1 * RotationSpeed * Time.deltaTime);
-            }
+        //    if (Input.GetKey(KeyCode.A))
+        //    {
+        //        transform.Rotate(transform.up * -1 * RotationSpeed * Time.deltaTime);
+        //    }
 
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Rotate(transform.up * RotationSpeed * Time.deltaTime);
-            }
+        //    if (Input.GetKey(KeyCode.D))
+        //    {
+        //        transform.Rotate(transform.up * RotationSpeed * Time.deltaTime);
+        //    }
 
-            if (Input.GetKey(KeyCode.W))
-            {
-                rb.AddForce(transform.forward * MovementForce * Time.deltaTime);
-            }
+        //    if (Input.GetKey(KeyCode.W))
+        //    {
+        //        rb.AddForce(transform.forward * MovementForce * Time.deltaTime);
+        //    }
 
-            if (Input.GetKey(KeyCode.S))
-            {
-                rb.AddForce(transform.forward * -1 * MovementForce * Time.deltaTime);
-            }
+        //    if (Input.GetKey(KeyCode.S))
+        //    {
+        //        rb.AddForce(transform.forward * -1 * MovementForce * Time.deltaTime);
+        //    }
 
-            #endregion
-        }
+        //    #endregion
+        //}
     }
 
-    public void ToggleControl(bool toggle)
-    {
-        isBeingControlled = toggle;
-        camera.SetActive(toggle);
-    }
+
 }
